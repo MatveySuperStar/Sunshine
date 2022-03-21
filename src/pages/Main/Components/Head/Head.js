c62ff79d8701a9ed90dcc955cae4101e519cfd36
MatveySuperStar <sergeev2000matvey@mail.ru>
2022-03-21 18:25:00 +0300

    init project

---
+++ src/pages/Main/Components/Head/Head.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Head.scss';
+
+const Head = () => {
+
+  const linksData = []
+
+  return (
+    <section>
+      <nav>
+        
+      </nav>
+      <div className='waves'></div>
+    </section>
+  );
+};
+
+export default Head;
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require("path")
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: "development",
+  entry: ['babel-polyfill', path.resolve(__dirname, 'src', 'index.js')],
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.js$/, 
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-syntax-jsx', 
+            '@babel/plugin-transform-classes',
+            '@babel/plugin-proposal-export-default-from',
+            '@babel/plugin-syntax-export-default-from']
+          },
+        },
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env','@babel/preset-react'],
+            plugins: ['@babel/plugin-syntax-jsx', 
+            '@babel/plugin-transform-classes',
+            '@babel/plugin-proposal-export-default-from',
+            '@babel/plugin-syntax-export-default-from']
+          },
+        },
+      },
+      {
+        test: /\.(css)$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "build.js"
+  },
+  devServer: {
+    port: 3000 
+  },
+  plugins: [
+    new HTMLWebpackPlugin({template: "./public/index.html"}),
+    new CleanWebpackPlugin()
+  ],
+}
