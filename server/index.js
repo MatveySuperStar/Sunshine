d0573c74a4d12b2604738693a2d7edae0fa643e5
MatveySuperStar <sergeev2000matvey@mail.ru>
2022-06-11 01:11:18 +0300

    added voice and files

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ const errorMiddleware = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(fileUpload())
 app.use(cors({
   credentials: true,
